fca46109a312e35932373e5e7f4943f36b7835d3
gt.park <smileboy0014@gmail.com>
2025-07-04 23:37:06 +0900

    feat: 소셜 로그인 시 refreshToken 저장/갱신 로직 추가

--- core/src/main/kotlin/com/imagesprint/core/service/user/SocialLoginService.kt
+++ core/src/main/kotlin/com/imagesprint/core/service/user/SocialLoginService.kt
@@ -1,13 +1,14 @@
 package com.imagesprint.core.service.user
 
+import com.imagesprint.core.domain.user.Token
 import com.imagesprint.core.domain.user.User
-import com.imagesprint.core.domain.user.UserRepository
 import com.imagesprint.core.port.input.user.SocialAuthCommand
 import com.imagesprint.core.port.input.user.SocialLoginUseCase
 import com.imagesprint.core.port.input.user.TokenResult
-import com.imagesprint.core.port.output.token.RefreshTokenStore
 import com.imagesprint.core.port.output.token.TokenProvider
+import com.imagesprint.core.port.output.token.TokenRepository
 import com.imagesprint.core.port.output.user.SocialAuthPortResolver
+import com.imagesprint.core.port.output.user.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -16,7 +17,7 @@ class SocialLoginService(
     private val userRepository: UserRepository,
     private val socialAuthPortResolver: SocialAuthPortResolver,
     private val tokenProvider: TokenProvider,
-    private val refreshTokenStore: RefreshTokenStore,
+    private val tokenRepository: TokenRepository,
 ) : SocialLoginUseCase {
     @Transactional
     override fun loginWithSocial(command: SocialAuthCommand): TokenResult {
@@ -35,7 +36,19 @@ class SocialLoginService(
         val accessToken = tokenProvider.generateAccessToken(userInfo.userId!!, userInfo.provider.name)
         val refreshToken = tokenProvider.generateRefreshToken(userInfo.userId, userInfo.provider.name)
 
-        refreshTokenStore.save(userInfo.userId, refreshToken)
+        val token =
+            tokenRepository.getRefreshToken(userId = userInfo.userId)
+
+        when {
+            token == null -> {
+                tokenRepository.save(Token(userId = userInfo.userId, refreshToken = refreshToken))
+            }
+
+            token.refreshToken.isBlank() -> {
+                val refreshed = token.refreshWith(refreshToken)
+                tokenRepository.save(refreshed)
+            }
+        }
 
         return TokenResult(accessToken, refreshToken)
     }
