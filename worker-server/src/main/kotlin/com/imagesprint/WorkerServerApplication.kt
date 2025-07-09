db73838a1f103789bc5bd928c9ed5f7b89b21d30
gt.park <smileboy0014@gmail.com>
2025-07-09 21:23:43 +0900

    feat: Worker 서버 실행 시 Redis 큐 소비 루프 자동 시작

--- worker-server/src/main/kotlin/com/imagesprint/WorkerServerApplication.kt
+++ worker-server/src/main/kotlin/com/imagesprint/WorkerServerApplication.kt
@@ -1,25 +1,19 @@
 package com.imagesprint
 
+import com.imagesprint.workerserver.consumer.JobQueueConsumer
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.FilterType
 
 @SpringBootApplication
-@ComponentScan(
-    basePackages = ["com.imagesprint"],
-    excludeFilters = [
-        ComponentScan.Filter(
-            type = FilterType.REGEX,
-            pattern = ["com\\.imagesprint\\.core\\.service\\.user\\..*"]
-        )
-    ]
-)
 class WorkerServerApplication
 
 fun main(args: Array<String>) {
-    runApplication<WorkerServerApplication>(*args) {
-        // 웹 서버 없이 실행하도록 설정하는 옵션, 워커라서 굳이 웹서버 띄울 필요가 없음
-        webApplicationType = org.springframework.boot.WebApplicationType.NONE
+    runBlocking {
+        val context = runApplication<WorkerServerApplication>(*args)
+        context.getBean(JobQueueConsumer::class.java).consumeLoop()
+        // 여기를 막아두면 JVM이 종료되지 않음
+        awaitCancellation()
     }
 }
