c578468027004dee8f196ad31af24895a723cd17
gt.park <smileboy0014@gmail.com>
2025-07-09 21:24:44 +0900

    chore: .gitignore 제외 대상 추가

--- infrastructure-jpa/src/main/kotlin/com/imagesprint/infrastructure/webhook/external/DefaultWebhookSender.kt
+++ infrastructure-jpa/src/main/kotlin/com/imagesprint/infrastructure/webhook/external/DefaultWebhookSender.kt
@@ -1,16 +1,17 @@
 package com.imagesprint.infrastructure.webhook.external
 
 import com.imagesprint.core.domain.webhook.Webhook
+import com.imagesprint.core.port.output.webhook.WebhookSender
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
 @Component
-class WebhookSender(
+class DefaultWebhookSender(
     private val webClient: WebClient,
     private val messageFactory: WebhookMessageFactory,
-) {
-    fun send(
+) : WebhookSender {
+    override fun send(
         webhook: Webhook,
         message: String,
     ) {
