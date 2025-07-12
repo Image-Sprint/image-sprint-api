f5c25542e6c1d71d1d683e7443ba282046877b55
gt.park <smileboy0014@gmail.com>
2025-07-13 08:43:32 +0900

    feat: S3 업로드용 presigned URL 생성 메서드 추가

--- worker-server/src/main/kotlin/com/imagesprint/workerserver/client/S3ClientAdapter.kt
+++ worker-server/src/main/kotlin/com/imagesprint/workerserver/client/S3ClientAdapter.kt
@@ -3,9 +3,10 @@ package com.imagesprint.workerserver.client
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
-import java.net.URL
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest
 import java.time.Duration
 
 @Component
@@ -13,12 +14,48 @@ class S3ClientAdapter(
     private val s3Presigner: S3Presigner,
     @Value("\${aws.s3.bucket}") private val bucket: String,
 ) {
-    // Presigned GET URL 발급 (다운로드용)
-    fun generatePresignedUrl(
+    private fun buildObjectKey(
         userId: Long,
         jobId: Long,
+    ): String = "users/$userId/jobs/$jobId/result.zip"
+
+    /**
+     * 업로드용 Presigned PUT URL
+     */
+    fun generatePresignedUploadUrl(
+        userId: Long,
+        jobId: Long,
+        validDuration: Duration = Duration.ofMinutes(5), // 업로드는 짧게(5분) 설정
     ): String {
-        val key = "users/$userId/jobs/$jobId/result.zip"
+        val key = buildObjectKey(userId, jobId)
+
+        val putObjectRequest =
+            PutObjectRequest
+                .builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType("application/zip")
+                .build()
+
+        val presignRequest =
+            PutObjectPresignRequest
+                .builder()
+                .putObjectRequest(putObjectRequest)
+                .signatureDuration(validDuration)
+                .build()
+
+        return s3Presigner.presignPutObject(presignRequest).url().toString()
+    }
+
+    /**
+     * 다운로드용 Presigned GET URL
+     */
+    fun generatePresignedDownloadUrl(
+        userId: Long,
+        jobId: Long,
+        validDuration: Duration = Duration.ofDays(1), // 다운로드는 길게(1일) 설정
+    ): String {
+        val key = buildObjectKey(userId, jobId)
 
         val getObjectRequest =
             GetObjectRequest
@@ -31,10 +68,9 @@ class S3ClientAdapter(
             GetObjectPresignRequest
                 .builder()
                 .getObjectRequest(getObjectRequest)
-                .signatureDuration(Duration.ofDays(1))
+                .signatureDuration(validDuration)
                 .build()
 
-        val presignedUrl: URL = s3Presigner.presignGetObject(presignRequest).url()
-        return presignedUrl.toString()
+        return s3Presigner.presignGetObject(presignRequest).url().toString()
     }
 }
